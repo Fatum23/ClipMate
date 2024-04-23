a8c641603a974398350c484c4f594bc5c0b0a936
Fatum23 <dmitrijnaumov342@gmail.com>
2024-04-23 20:17:35 +0300

    idk

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,24 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{Manager, PhysicalPosition};
 
 fn main() {
     tauri::Builder::default()
+        .setup(|app| {
+            let win = app.get_webview_window("main").unwrap();
+
+            let monitor = win.current_monitor().unwrap();
+            if let Some(monitor) = monitor {
+                win.set_position(PhysicalPosition {
+                    x: monitor.size().width - 385,
+                    y: monitor.size().height - 560,
+                })
+                .unwrap();
+            }
+            Ok(())
+        })
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles.css";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+	<React.StrictMode>
+		<div className='text-white bg-red-500'>Hello World!</div>
+	</React.StrictMode>
+)
